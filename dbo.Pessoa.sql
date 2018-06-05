06ff1e4cf5ea226a36afd4fa3d82868ad082e118
Maurilio <msilva1610@gmail.com>
2018-06-04 22:50:35 -0300

    db

---
+++ dbo.Pessoa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Pessoa]
+(
+	[Codigo] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NCHAR(50) NOT NULL, 
+    [Endereco] NCHAR(50) NOT NULL, 
+    [Email] NCHAR(50) NOT NULL
+)
